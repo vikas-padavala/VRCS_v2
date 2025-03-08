147d0aa5cc226cdb2e11492e7a39ec3ec20d3034
Vikas <vikas.padavala@gmail.com>
2025-03-08 18:04:49 +0000

    initial commit for vijaya service

---
+++ vijaya/src/test/java/com/vrcs/vijaya/VijayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrcs.vijaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VijayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
