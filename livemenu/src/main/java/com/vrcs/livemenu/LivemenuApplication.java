f05668a5c08440db5f9b9c145e30d37afd159934
Vikas <vikas.padavala@gmail.com>
2025-03-08 18:00:43 +0000

    initial commit

---
+++ livemenu/src/main/java/com/vrcs/livemenu/LivemenuApplication.java
@@ -0,0 +1,13 @@
+package com.vrcs.livemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivemenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivemenuApplication.class, args);
+	}
+
+}
---
+++ livemenu/src/test/java/com/vrcs/livemenu/LivemenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrcs.livemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivemenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
